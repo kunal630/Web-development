5c7fd6ddc18cd8b5f8211220c10fd012602e4a48
kunal630 <pundekuna2008@gmail.com>
2026-07-11 10:13:05 +0530

    added random color to baoxes

---
+++ randomcolor/scipt.js
@@ -0,0 +1,13 @@
+console.log("Helloe developers");
+let boxes=document.querySelector(".container").children;
+console.log(boxes);
+function getrandomColor(){
+    let var1=Math.ceil(0+Math.random()*255);
+    let var2=Math.ceil(0+Math.random()*255);
+    let var3=Math.ceil(0+Math.random()*255);
+    return `rgb(${var1}, ${var2}, ${var3})`
+}
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getrandomColor()
+    e.style.color = getrandomColor()
+})
